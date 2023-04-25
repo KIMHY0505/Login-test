d9701135759c4350e7c2dad7cc4e0ed455b387f4
MinJun Kim <kimminjuw@gmail.com>
2023-04-25 18:06:39 +0900

    Fetch-updater

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const home = require("./src/index");
 const PORT = 3000;
 
 app.set("view engine", "ejs");
-app.set("views", "./ejs");
+app.set("views", "./src/ejs");
+app.use(express.static(`${__dirname}/src/ejs`));
 
 app.use("/", home);
 
---
+++ src/ejs/js/login.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const id = document.querySelector("#ID");
+const pw = document.querySelector("#PW");
+const btn = document.querySelector("btn");
+
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+}
